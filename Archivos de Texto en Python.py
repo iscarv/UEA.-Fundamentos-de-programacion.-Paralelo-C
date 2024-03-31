878c07435e2567953db9f3540a48015b3a1d4926
iscarv <im.ortizs@uea.edu.ec>
2024-03-31 14:28:08 -0500

    Versión final

---
+++ Archivos de Texto en Python.py
@@ -0,0 +1,17 @@
+# 1. Escritura de Archivo de Texto:
+with open("my_notes.txt", "w") as archivo:
+# Escribe al menos tres líneas de notas personales en el archivo.
+     archivo.write("Notas personales:\n")
+     archivo.write("1. Terminar el proyecto de Inglés.\n")
+     archivo.write("2. Arreglar mi cuarto.\n")
+     archivo.write("3. Correguir la tarea de matemáticas.\n")
+
+# 2. Lectura de Archivo de Texto:
+archivo = open("my_notes.txt", "r")
+# Leer e imprimir cada línea del archivo
+linea = archivo.readline()  # Leer la primera línea
+while linea:
+    print(linea.strip())  # Mostrar en la consola cada línea leída
+    linea = archivo.readline()  # Leer la siguiente línea
+# 3. Cerrar el archivo después de realizar las operaciones necesarias
+archivo.close()
